fec31f97f6f0b0d1cc5e3a15fb3e739ed72bf66f
Irina Rysbaeva <irsbv@mail.ru>
2021-05-28 23:58:57 +0500

    change routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./components/App";
 import ErrorBoundry from "./components/error-boundry";
 import store from "./store";
 import { ToastContainer } from "react-toastify";
+import Login from "./components/login";
 
 ReactDOM.render(
   // <React.StrictMode>
   <Provider store={store}>
     <ErrorBoundry>
       <Router>
+        <Route path="/login">
+          <Login />
+        </Route>
         <ToastContainer />
         <App />
       </Router>
